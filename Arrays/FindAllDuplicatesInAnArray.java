8cab3fab1fb186e0c4d7cdaaaa08db4bd79c36b7
Vikanth <yvikanth817@gmail.com>
2024-07-17 19:56:39 +0530

    Day01-CrackYourPlacement

---
+++ Arrays/FindAllDuplicatesInAnArray.java
@@ -0,0 +1,12 @@
+package Arrays;
+import java.util.*;
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> lst = new ArrayList<>();
+        for(int i=0; i<nums.length; i++){
+            if(nums[Math.abs(nums[i])-1]<0)lst.add(Math.abs(nums[i]));
+            nums[Math.abs(nums[i])-1]*=-1;
+        }
+        return lst;
+    }
+}
